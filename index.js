e416802e1bcab11403dd9ea1c5fdf273c21e10d6
Evgen Redka <evgenr946@gmail.com>
2023-05-25 12:39:39 +0200

    upload file

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import express from "express";
+import multer from "multer";
+
 
 import cors from "cors";
 import mongoose from 'mongoose';
@@ -18,19 +20,37 @@ mongoose.connect('mongodb+srv://root:root@cluster0.ctmgvju.mongodb.net/?retryWri
 
 const app =express();
 
+const storage = multer.diskStorage({
+    destination: (_,__, cb)=>{
+        cb(null,'uploads');
+    },
+    filename:(_, file,cb)=>{
+        cb(null,file.originalname);
+    }
+});
+
+const upload = multer({ storage });
+
 app.use(cors())
 
 app.use(express.json());
+app.use('/uploads', express.static('uploads'));
 
 
 app.post('/auth/login',loginValidation, UserController.login);
 app.post('/auth/register',registerValidation,UserController.register);
 app.get('/auth/me', checkAuth, UserController.getMe);
 
+app.post('/upload',upload.single('image'),(req,res)=>{
+    res.json({
+        url: `/uploads/${req.file.originalname}`,
+    });
+}) 
+
 app.get('/products', ProductsController.getAll); // For All Product
 app.get('/products/:id', ProductsController.getOne); // For One product
 // For admin
-app.post('/products', checkAuth , productCreateValidation, ProductsController.create);
+app.post('/products', checkAuth ,upload.single('image') , productCreateValidation, ProductsController.create);
 app.delete('/products/:id', checkAuth , ProductsController.remove);
 app.patch('/products/:id', ProductsController.update);
 
